1b28cd266985a362d22b6df26291ce1205e77e58
Junjie Yang <yangjunjie0320@gmail.com>
2025-02-08 00:09:39 -0800

    Optimize AVX2 packing-based matrix multiplication implementation
    - Refactor dgemm-8x4.c with improved packing and macro kernel functions
    - Add alpha and beta scaling support for matrix multiplication
    - Modify memory access patterns and indexing for better performance
    - Update blocking parameters and memory allocation strategy
    - Enhance error handling and matrix multiplication computation

--- src/avx2-packing/dgemm-8x4.c
+++ src/avx2-packing/dgemm-8x4.c
@@ -1,17 +1,33 @@
-// #include <cstdio>
-#include <immintrin.h>
+#include "immintrin.h"
+#define A(i,j) A[(i)+(j)*LDA]
+#define B(i,j) B[(i)+(j)*LDB]
+#define C(i,j) C[(i)+(j)*LDC]
+#define M_BLOCKING 192
+#define N_BLOCKING 2048
+#define K_BLOCKING 384
+void scale_c_k9(double *C,int M, int N, int LDC, double scalar){
+    int i,j;
+    for (i=0;i<M;i++){
+        for (j=0;j<N;j++){
+            C(i,j)*=scalar;
+        }
+    }
+}
 
-#define min( i, j ) ( (i)<(j) ? (i): (j) )
-#define BLOCK_SIZE_M 8
-#define BLOCK_SIZE_N 4
-#define UNROLL 4
-
-void packing_a(double *src, double *dst, int leading_dim, int dim_first, int dim_second){
+void packing_a_k9(double *src, double *dst, int leading_dim, int dim_first, int dim_second){
     //dim_first: M, dim_second: K
     double *tosrc,*todst;
     todst=dst;
     int count_first,count_second,count_sub=dim_first;
-    for (count_first=0;count_sub>3;count_first+=4,count_sub-=4){
+    for (count_first=0;count_sub>7;count_first+=8,count_sub-=8){
+        tosrc=src+count_first;
+        for(count_second=0;count_second<dim_second;count_second++){
+            _mm512_store_pd(todst,_mm512_loadu_pd(tosrc));
+            tosrc+=leading_dim;
+            todst+=8;
+        }
+    }
+    for (;count_sub>3;count_first+=4,count_sub-=4){
         tosrc=src+count_first;
         for(count_second=0;count_second<dim_second;count_second++){
             _mm256_store_pd(todst,_mm256_loadu_pd(tosrc));
@@ -19,17 +35,9 @@ void packing_a(double *src, double *dst, int leading_dim, int dim_first, int dim
             todst+=4;
         }
     }
-    for (;count_sub>0;count_first++,count_sub--){
-        tosrc=src+count_first;
-        for(count_second=0;count_second<dim_second;count_second++){
-            *todst=*tosrc;
-            tosrc+=leading_dim;
-            todst++;
-        }
-    }
 }
 
-void packing_b(double *src,double *dst,int leading_dim,int dim_first,int dim_second){
+void packing_b_k9(double *src,double *dst,int leading_dim,int dim_first,int dim_second){
     //dim_first:K,dim_second:N
     double *tosrc1,*tosrc2,*tosrc3,*tosrc4,*todst;
     todst=dst;
@@ -46,23 +54,19 @@ void packing_b(double *src,double *dst,int leading_dim,int dim_first,int dim_sec
     }
 }
 
-void edge_block(int m, int n, int l, int lda, int ldb, int ldc, double* a, double* b, double* c) {
-    int i, j, k;
-
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            double cij = c[i + j * ldc];
-
-            for (k = 0; k < l; k++) {
-                double aik = a[i + k * lda];
-                double bkj = b[k + j * ldb];
-                cij += aik * bkj;
+void mydgemm_cpu_opt_k9(int M, int N, int K, double alpha, double *A, int LDA, double *B, int LDB, double beta, double *C, int LDC){
+    int i,j,k;
+    if (beta != 1.0) scale_c_k9(C,M,N,LDC,beta);
+    for (i=0;i<M;i++){
+        for (j=0;j<N;j++){
+            double tmp=C(i,j);
+            for (k=0;k<K;k++){
+                tmp += alpha*A(i,k)*B(k,j);
             }
-            c[i + j * ldc] = cij;
+            C(i,j) = tmp;
         }
     }
 }
-
 
 #define KERNEL_K1_8x4_avx2_intrinsics_packing\
     a0 = _mm256_mul_pd(valpha, _mm256_load_pd(ptr_packing_a));\
@@ -98,14 +102,14 @@ void edge_block(int m, int n, int l, int lda, int ldb, int ldc, double* a, doubl
     for (k=K4;k<K;){\
         KERNEL_K1_8x4_avx2_intrinsics_packing\
     }\
-    _mm256_storeu_pd(c + (i + 0) * ldc + (j + 0), _mm256_add_pd(c00,_mm256_loadu_pd(c + (i + 0) * ldc + (j + 0))));\
-    _mm256_storeu_pd(c + (i + 4) * ldc + (j + 0), _mm256_add_pd(c01,_mm256_loadu_pd(c + (i + 4) * ldc + (j + 0))));\
-    _mm256_storeu_pd(c + (i + 0) * ldc + (j + 1), _mm256_add_pd(c10,_mm256_loadu_pd(c + (i + 0) * ldc + (j + 1))));\
-    _mm256_storeu_pd(c + (i + 4) * ldc + (j + 1), _mm256_add_pd(c11,_mm256_loadu_pd(c + (i + 4) * ldc + (j + 1))));\
-    _mm256_storeu_pd(c + (i + 0) * ldc + (j + 2), _mm256_add_pd(c20,_mm256_loadu_pd(c + (i + 0) * ldc + (j + 2))));\
-    _mm256_storeu_pd(c + (i + 4) * ldc + (j + 2), _mm256_add_pd(c21,_mm256_loadu_pd(c + (i + 4) * ldc + (j + 2))));\
-    _mm256_storeu_pd(c + (i + 0) * ldc + (j + 3), _mm256_add_pd(c30,_mm256_loadu_pd(c + (i + 0) * ldc + (j + 3))));\
-    _mm256_storeu_pd(c + (i + 4) * ldc + (j + 3), _mm256_add_pd(c31,_mm256_loadu_pd(c + (i + 4) * ldc + (j + 3))));
+    _mm256_storeu_pd(&C(i,j), _mm256_add_pd(c00,_mm256_loadu_pd(&C(i,j))));\
+    _mm256_storeu_pd(&C(i+4,j), _mm256_add_pd(c01,_mm256_loadu_pd(&C(i+4,j))));\
+    _mm256_storeu_pd(&C(i,j+1), _mm256_add_pd(c10,_mm256_loadu_pd(&C(i,j+1))));\
+    _mm256_storeu_pd(&C(i+4,j+1), _mm256_add_pd(c11,_mm256_loadu_pd(&C(i+4,j+1))));\
+    _mm256_storeu_pd(&C(i,j+2), _mm256_add_pd(c20,_mm256_loadu_pd(&C(i,j+2))));\
+    _mm256_storeu_pd(&C(i+4,j+2), _mm256_add_pd(c21,_mm256_loadu_pd(&C(i+4,j+2))));\
+    _mm256_storeu_pd(&C(i,j+3), _mm256_add_pd(c30,_mm256_loadu_pd(&C(i,j+3))));\
+    _mm256_storeu_pd(&C(i+4,j+3), _mm256_add_pd(c31,_mm256_loadu_pd(&C(i+4,j+3))));
 #define KERNEL_K1_4x4_avx2_intrinsics_packing\
     a0 = _mm256_mul_pd(valpha, _mm256_load_pd(ptr_packing_a));\
     b0 = _mm256_broadcast_sd(ptr_packing_b);\
@@ -131,21 +135,15 @@ void edge_block(int m, int n, int l, int lda, int ldb, int ldc, double* a, doubl
     for (k=K4;k<K;){\
         KERNEL_K1_4x4_avx2_intrinsics_packing\
     }\
-    _mm256_storeu_pd(c + i * ldc + (j + 0), _mm256_add_pd(c00,_mm256_loadu_pd(c + i * ldc + (j + 0))));\
-    _mm256_storeu_pd(c + i * ldc + (j + 1), _mm256_add_pd(c10,_mm256_loadu_pd(c + i * ldc + (j + 1))));\
-    _mm256_storeu_pd(c + i * ldc + (j + 2), _mm256_add_pd(c20,_mm256_loadu_pd(c + i * ldc + (j + 2))));\
-    _mm256_storeu_pd(c + i * ldc + (j + 3), _mm256_add_pd(c30,_mm256_loadu_pd(c + i * ldc + (j + 3))));
-
-void macro_kernel(int M, int N, int K, double *A, int LDA, double *B, int LDB, double *C, int LDC){
+    _mm256_storeu_pd(&C(i,j), _mm256_add_pd(c00,_mm256_loadu_pd(&C(i,j))));\
+    _mm256_storeu_pd(&C(i,j+1), _mm256_add_pd(c10,_mm256_loadu_pd(&C(i,j+1))));\
+    _mm256_storeu_pd(&C(i,j+2), _mm256_add_pd(c20,_mm256_loadu_pd(&C(i,j+2))));\
+    _mm256_storeu_pd(&C(i,j+3), _mm256_add_pd(c30,_mm256_loadu_pd(&C(i,j+3))));
+void macro_kernel_gemm_k9(int M, int N, int K, double alpha, double *A, int LDA, double *B, int LDB, double *C, int LDC){
     int i,j,k;
     int M8=M&-8,N4=N&-4,K4=K&-4;
     double *ptr_packing_a = A;
     double *ptr_packing_b = B;
-
-    double* c = C;
-    int ldc = LDC;
-
-    double alpha = 1.0;
     __m256d valpha = _mm256_set1_pd(alpha);//broadcast alpha to a 256-bit vector
     __m256d a,a0,a1,b0,b1,b2,b3;
     __m256d c00,c01,c10,c11,c20,c21,c30,c31;
@@ -164,44 +162,25 @@ void macro_kernel(int M, int N, int K, double *A, int LDA, double *B, int LDB, d
     }
 }
 
-#define M_BLOCKING 192
-#define N_BLOCKING 2048
-#define K_BLOCKING 384
-
-void dgemm(int m, int n, int l, double* a, double* b, double* c) {
-    int m0 = m, n0 = n, l0 = l;
-    int m1 = 0, n1 = 0, l1 = 0;
-    int ii, jj, kk;
-
-    int lda = m;
-    int ldb = l;  
-    int ldc = m;
-
-    double* a_buffer = (double*) aligned_alloc(4096, K_BLOCKING * N_BLOCKING * sizeof(double));
-    double* b_buffer = (double*) aligned_alloc(4096, K_BLOCKING * N_BLOCKING * sizeof(double));
-
-    // n, l, m
-    for (jj = 0; jj < n0;) {
-        n1 = min(N_BLOCKING, n0 - jj);
-        for (kk = 0; kk < l0;) {
-            l1 = min(K_BLOCKING, l0 - kk);
-            packing_b(b + kk + jj * ldb, b_buffer, ldb, l1, n1);
-            for (ii = 0; ii < m0; ) {
-                m1 = min(M_BLOCKING, m0 - ii);
-                packing_a(a + ii + kk * lda, a_buffer, lda, l1, m1);
-                macro_kernel(
-                    m1, n1, l1,
-                    a_buffer, lda,
-                    b_buffer, ldb,
-                    c + ii + jj * ldc, ldc
-                );
-                ii += m1;
+void mydgemm_cpu_v9(int M, int N, int K, double alpha, double *A, int LDA, double *B, int LDB, double beta, double *C, int LDC){
+    
+    if (beta != 1.0) scale_c_k9(C,M,N,LDC,beta);
+    double *b_buffer = (double *)aligned_alloc(4096,K_BLOCKING*N_BLOCKING*sizeof(double));
+    double *a_buffer = (double *)aligned_alloc(4096,K_BLOCKING*M_BLOCKING*sizeof(double));
+    int m_count, n_count, k_count;
+    int m_inc, n_inc, k_inc;
+    for (n_count=0;n_count<N;n_count+=n_inc){
+        n_inc = (N-n_count>N_BLOCKING)?N_BLOCKING:N-n_count;
+        for (k_count=0;k_count<K;k_count+=k_inc){
+            k_inc = (K-k_count>K_BLOCKING)?K_BLOCKING:K-k_count;
+            packing_b_k9(B+k_count+n_count*LDB,b_buffer,LDB,k_inc,n_inc);
+            for (m_count=0;m_count<M;m_count+=m_inc){
+                m_inc = (M-m_count>M_BLOCKING)?M_BLOCKING:N-m_count;
+                packing_a_k9(A+m_count+k_count*LDA,a_buffer,LDA,m_inc,k_inc);
+                //macro kernel: to compute C += A_tilt * B_tilt
+                macro_kernel_gemm_k9(m_inc,n_inc,k_inc,alpha,a_buffer, LDA, b_buffer, LDB, &C(m_count, n_count), LDC);
             }
-            kk += l1;
         }
-        jj += n1;
     }
-
-    free(a_buffer);
-    free(b_buffer);
+    free(a_buffer);free(b_buffer);
 }
